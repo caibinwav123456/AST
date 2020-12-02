fbf37d751d2095758466a5464b442b1880441208
CaiBin <342571403@qq.com>
2020-12-02 12:53:00 +0800

    Expanded utilities of int64.

--- IPCIF/include/Integer64.h
+++ IPCIF/include/Integer64.h
@@ -3,6 +3,7 @@
 #include "common.h"
 #include <string>
 using namespace std;
+#pragma pack(push,8)
 template<class T>
 struct DLL _Integer64
 {
@@ -10,10 +11,17 @@ struct DLL _Integer64
 	uint low;
 	_Integer64(int l=0,const T* h=NULL);
 };
+typedef _Integer64<int> Integer64;
+typedef _Integer64<uint> UInteger64;
+#pragma pack(pop)
+template<class T>
+DLL _Integer64<T> operator-(const _Integer64<T>& a);
 template<class T>
 DLL _Integer64<T> operator+(const _Integer64<T>& a,const _Integer64<T>& b);
 template<class T>
 DLL _Integer64<T> operator-(const _Integer64<T>& a,const _Integer64<T>& b);
+template<class T>
+DLL _Integer64<T> Mul64(const _Integer64<T>& a,const _Integer64<T>& b,_Integer64<T>* phigh=NULL);
 template<class T>
 DLL bool operator==(const _Integer64<T>& a,const _Integer64<T>& b);
 template<class T>
@@ -23,7 +31,11 @@ DLL bool operator<(const _Integer64<T>& a,const _Integer64<T>& b);
 template<class T>
 DLL bool operator>(const _Integer64<T>& a,const _Integer64<T>& b);
 template<class T>
-DLL string FormatI64(_Integer64<T> i);
-typedef _Integer64<int> Integer64;
-typedef _Integer64<uint> UInteger64;
+DLL string FormatI64(const _Integer64<T>& i);
+template<class T>
+DLL string FormatI64Hex(const _Integer64<T>& i);
+template<class T>
+DLL bool I64FromDec(const string& dec,_Integer64<T>& i);
+template<class T>
+DLL bool I64FromHex(const string& hex,_Integer64<T>& i);
 #endif
