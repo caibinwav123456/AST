29ee78c8fca73454a62ebdecb9073c3d1bbec936
CaiBin <342571403@qq.com>
2021-10-18 07:21:13 +0800

    Added & modified error code.

--- IPCIF/src/common/config.cpp
+++ IPCIF/src/common/config.cpp
@@ -168,7 +168,7 @@ int ConfigProfile::LoadConfigFile(const char* filename)
 	byte* buf;
 	void* hFile=sys_fopen(const_cast<char*>(filename),FILE_OPEN_EXISTING|FILE_READ);
 	if(!VALID(hFile))
-		return ERR_FILE_IO;
+		return ERR_OPEN_FILE_FAILED;
 	if(0!=(ret=sys_get_file_size(hFile,&filesize)))
 	{
 		goto final;
