64e060558761fac671c9bda507a3f7159e13e4e1
Bin Cai <46627288+caibinwav123456@users.noreply.github.com>
2021-11-10 21:58:51 +0800

    Update gen_var.sh

--- misc/tools/gen_var/gen_var.sh
+++ misc/tools/gen_var/gen_var.sh
@@ -3,7 +3,7 @@ for file in `tree -nif --noreport IPCIF`;
   if [ -f "$file" ]; then
    d=`cat "$file" | grep ^DEFINE_`;
    if [ -n "$d" ]; then
-    if [ "$1" = "-v" ]; then
+    if [ "$1" == "-v" ]; then
      echo "$file":
     fi;
     echo "$d";
